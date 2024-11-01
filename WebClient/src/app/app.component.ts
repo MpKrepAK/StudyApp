0f527b567a98d5ef84f92f75bb58e5b4c1ab2d22
MpKrepAK <vova.nesteruk.90@mail.ru>
2024-11-01 16:17:34 +0300

    AppComp changes

--- WebClient/src/app/app.component.ts
+++ WebClient/src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from './header/header.component';
+import {LecturesSubjectsComponent} from './lectures-subjects/lectures-subjects.component';
+import {LecturesComponent} from './lectures/lectures.component';
+import {LecturePageComponent} from './lectures/lecture-page/lecture-page.component';
+import {LectureGroupComponent} from './lectures/lecture-group/lecture-group.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, LecturesSubjectsComponent, LecturesComponent, LecturePageComponent, LectureGroupComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
